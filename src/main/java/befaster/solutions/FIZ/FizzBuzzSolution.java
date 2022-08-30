bd900492075644261b9ad94e983c005c682e34e6
sbx_user1051 <sbx_user1051@169.254.226.221>
2022-08-30 10:14:05 +0000

    Tue Aug 30 10:14:05 UTC 2022

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,17 +3,32 @@ package befaster.solutions.FIZ;
 import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
-
+    /**
+     - A number is "fizz" if it is divisible by 3 or if it has a 3 in it
+     - A number is "buzz" if it is divisible by 5 or if it has a 5 in it
+     - A number can be both "fizz" and "buzz" at the same time. If this happens then write "fizz buzz"
+     */
     public String fizzBuzz(Integer number) {
-        if (number % 3 == 0 && number % 5 == 0){
+        if ((number % 3 == 0 || hasNumber(number, 3)) && (number % 5 == 0 || hasNumber(number, 5))){
             return "fizz buzz";
-        } else if (number % 3 == 0) {
+        } else if (number % 3 == 0 || hasNumber(number, 3)) {
             return "fizz";
-        } else if (number % 5  == 0) {
+        } else if (number % 5  == 0 || hasNumber(number, 5)) {
             return "buzz";
         } else{
             return number.toString();
         }
     }
+
+    public Boolean hasNumber(Integer num, Integer digit){
+        while(num > 0){
+            if(num % 10 == digit){
+                return true;
+            }
+            num = num / 10;
+        }
+        return false;
+    }
 }
+
 
