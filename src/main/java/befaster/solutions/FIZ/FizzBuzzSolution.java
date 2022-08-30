f324ffc3a87836a7f0d4a3c0c46ce30788a6b3c5
sbx_user1051 <sbx_user1051@169.254.226.221>
2022-08-30 10:22:17 +0000

    Tue Aug 30 10:22:17 UTC 2022

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,13 +4,23 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
     /**
-     - A number is "fizz" if it is divisible by 3 or if it has a 3 in it
-     - A number is "buzz" if it is divisible by 5 or if it has a 5 in it
-     - A number can be both "fizz" and "buzz" at the same time. If this happens then write "fizz buzz"
+     - A number is considered to be "deluxe" if it is greater than 10 and all the digits are identical
+     - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
      */
     public String fizzBuzz(Integer number) {
-        if ((number % 3 == 0 || hasNumber(number, 3)) && (number % 5 == 0 || hasNumber(number, 5))){
+        if ((number % 3 == 0 || hasNumber(number, 3)) && (number % 5 == 0 || hasNumber(number, 5)) &&
+            hasIdenticalDigits(number) && number > 10){
+            return "fizz buzz deluxe";
+        } else if ((number % 3 == 0 || hasNumber(number, 3)) &&
+                hasIdenticalDigits(number) && number > 10){
+            return "fizz deluxe";
+        } else if ((number % 5 == 0 || hasNumber(number, 5)) &&
+                hasIdenticalDigits(number) && number > 10) {
+            return "buzz deluxe";
+        } else if ((number % 3 == 0 || hasNumber(number, 3)) && (number % 5 == 0 || hasNumber(number, 5))) {
             return "fizz buzz";
+        } else if (hasIdenticalDigits(number) && number > 10) {
+            return "deluxe";
         } else if (number % 3 == 0 || hasNumber(number, 3)) {
             return "fizz";
         } else if (number % 5  == 0 || hasNumber(number, 5)) {
@@ -29,4 +39,23 @@ public class FizzBuzzSolution {
         }
         return false;
     }
+
+    public Boolean hasIdenticalDigits(Integer num){
+        Integer digit = num % 10;
+        Integer check = 0;
+        Integer numOfDigits = 0;
+        while(num > 0){
+            if(num % 10 == digit){
+                check += 1;
+            }
+            numOfDigits += 1;
+            num = num / 10;
+        }
+        if(numOfDigits == check){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
+
